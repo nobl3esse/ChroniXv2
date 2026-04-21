6a162b9673ece29d5601d10080caf89176c1a0d6
Noblesse <104496903+nobl3esse@users.noreply.github.com>
2026-04-21 22:03:41 +0200

    Dokumentiert

--- apps/backend/api/ChroniXApi/Services/WhitelistService.cs
+++ apps/backend/api/ChroniXApi/Services/WhitelistService.cs
@@ -5,11 +5,15 @@ namespace ChroniXApi.Services
 {
     public class WhitelistService
     {
+
         private List<string> allowedProcesses = new List<string>();
+
+        //Speichert den Pfad von Whitelist in einer konstanten Zeichenkette
         private const string FilePath = "whitelist.json";
 
         public class WhitelistData
         {
+            //Erstellt eine neue Liste aus der Liste Processes die durch get und set verändert werden kann 
             public List<string> Processes { get; set; } = new List<string>();
         }
 
@@ -21,10 +25,16 @@ namespace ChroniXApi.Services
                 return;
             }
 
+            //Ließt den Inhalt von Whitelist und speichert ihn in jsonString
             var jsonString = File.ReadAllText(FilePath);
+            //Erstellt eine neue Json Option die den Serializer Case Insensitive macht
             var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+
+            //Zerlegt die Json in ihre Wert und speichert sie in data
             var data = JsonSerializer.Deserialize<WhitelistData>(jsonString, options);
 
+            //Wenn data nicht leer oder zu einem Objekt gehört
+            //speicher die Processes aus data in allowedProcesses
             if (data != null)
             {
                 allowedProcesses = data.Processes;
